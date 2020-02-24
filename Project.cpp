2504b8e48273e4855ae62fe7fe46b77101fcdfec
Dmitry Osetrov (PC at Nmsk) <guise322@ya.ru>
2020-02-24 14:11:59 +0300

    Create a template of the project

---
+++ Project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string msg = "Hello";
+
+    
+        cout << msg;
+    
+    cout << endl;
+}
