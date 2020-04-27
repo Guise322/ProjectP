c98737c52a97940a916c26a14ac890e0da27b874
Dmitry's Personal Notebook <guise322@ya.ru>
2020-04-27 16:59:42 +0300

    Improve the process of file searching, wordsVector is copied to wordsToCompare to save upper letters

--- FileSearching.h
+++ FileSearching.h
@@ -18,12 +18,19 @@ public:
 
         //This statement converting upper case letters to lower case letters is occured below, because these letters don't equals to each other
         transform(wordForSearching.begin(), wordForSearching.end(), wordForSearching.begin(), ::tolower);
+        
+        vector<string> wordsToCompare = wordsVector;
+
+        for (string i : wordsToCompare)
+        {
+            transform(i.begin(), i.end(), i.begin(), ::tolower);
+        }
 
         string comparedWord = "";
 
         for (int i = 0; i < cnt; i++)
         {
-            if (wordForSearching.compare(wordsVector[i]) == 0)
+            if (wordForSearching.compare(wordsToCompare[i]) == 0)
             {
                 amountOfWords++;
                 comparedWord = to_string(i + 1);
@@ -46,5 +53,6 @@ public:
             cout << "The word number is " << wordNumber << endl;
         }
         cout << "Amount of words is " << amountOfWords << endl;
+        wordsToCompare.clear();
 	}
 };
