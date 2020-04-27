868fb90e6c2b8223580f48b6bce1b251d2d17424
Dmitry's Personal Notebook <guise322@ya.ru>
2020-04-27 16:49:17 +0300

    Improve the process asking the user

--- Project.cpp
+++ Project.cpp
@@ -21,7 +21,7 @@ int FileWritingReading::amountOfWords = 0;
 char valueForEnter;
 
 vector<string> FileWritingReading::wordsVector;
-string choosenMode = "";
+
 enum mode
 {
     writingForm, searchingInFile, writingTitleForm, writingIntoDict, idle
@@ -34,111 +34,115 @@ int main()
 
     do
     {
-    cout << "Choose a mode of work:" << endl << "1 - Replacing particular words in the text;"
-        << endl << "2 - Searching a word from the file;" << endl << "3 - Writing the title list form;"
-        <<endl<<"4 - Writing a memorandum template;"<< endl <<"5 - Write a word into the dictionary" << endl << endl;
-    cout << "Write the number of the mode: ";
-    
-    getline(cin, choosenMode);
+        cout << endl << "Choose a mode of work:" << endl << "1 - Replacing particular words in the text;"
+            << endl << "2 - Searching a word from the file;" << endl << "3 - Writing the title list form;"
+            << endl << "4 - Writing a memorandum template;" << endl << "5 - Write a word into the dictionary" << endl << endl;
+        cout << "Write the number of the mode: ";
 
-    if (choosenMode == "1")
-    {
-        currMode = mode::writingForm;
+        char choosenMode;
 
-        FileWritingReading fileWritingReading;
-        FileReplacement fileReplacement;
+        cin >> choosenMode;
 
-        cout << "Write into the text.txt file" << endl;
-
-        bool repeat = false;
-
-        while (!repeat)
-        //for (int i = 0; i < 60; i++)
+        if (choosenMode == '1')
         {
-            
-            FileWritingReading::cnt = 0;
-            FileWritingReading::wordsVector.resize(0);
-            this_thread::sleep_for(1s);
+            currMode = mode::writingForm;
 
-            fileWritingReading.readFromFile();
+            FileWritingReading fileWritingReading;
+            FileReplacement fileReplacement;
 
-            fileReplacement.wordReplacement();
+            cout << endl << "Write into the text.txt file" << endl;
 
-            fileWritingReading.writeToFile();
+            bool repeat = false;
 
-            while (true)
+            while (!repeat)
+                //for (int i = 0; i < 60; i++)
             {
-                cout << endl << "Repeate the process? [Y/n]\n";
-         
-                cin.get(ch);
 
-                if (ch == 'y' || ch == 'Y' || ch == '\n')
+                FileWritingReading::cnt = 0;
+                FileWritingReading::wordsVector.resize(0);
+                this_thread::sleep_for(1s);
+
+                fileWritingReading.readFromFile();
+
+                fileReplacement.wordReplacement();
+
+                fileWritingReading.writeToFile();
+
+                while (true)
                 {
-                    repeat = false;
-                    //Use this statement for clearing a cin buffer because next cin.get() takes a char '\n'
-                    cin.ignore(INT_MAX, '\n');
-                    break;
-                }
-                /*else if (ch == '\n')
-                {
-                    repeat = false;
-                    break;
-                }*/
-                else if (ch != 'n')
-                {
-                    cin.ignore(INT_MAX, '\n');
-                    cout << "\nError!\n\nRepeate the command\n";
-                    repeat = false;
-                }
-                else
-                {
-                    cin.ignore(INT_MAX, '\n');
-                    repeat = true;
-                    break;
+                    cout << endl << "Repeate the process? [Y/n]" << endl << endl;
+
+                    cin >> ch;
+
+                    if (ch == 'y' || ch == 'Y' || ch == '\n')
+                    {
+                        repeat = false;
+                        //Use this statement for clearing a cin buffer because next cin.get() takes a char '\n'
+                        //cin.ignore(INT_MAX, '\n');
+                        break;
+                    }
+                    /*else if (ch == '\n')
+                    {
+                        repeat = false;
+                        break;
+                    }*/
+                    else if (ch != 'n')
+                    {
+                        //cin.ignore(INT_MAX, '\n');
+                        cout << endl << "Error!" << endl << endl << "Repeate the command" << endl;
+                        repeat = false;
+                    }
+                    else
+                    {
+                        //cin.ignore(INT_MAX, '\n');
+                        repeat = true;
+                        break;
+                    }
                 }
             }
+
+            //FileSearch fileSearch;
+
+            //fileSearch.fileSearch();
         }
 
-        //FileSearch fileSearch;
+        else if (choosenMode == '3')
+        {
+            cout << endl;
 
-        //fileSearch.fileSearch();
-    }
+            currMode = mode::writingTitleForm;
 
-    else if (choosenMode == "3")
-    {
-        cout << endl;
+            TitleForm titleForm;
 
-        currMode = mode::writingTitleForm;
+            titleForm.writeTitleForm();
+        }
 
-        TitleForm titleForm;
+        else if (choosenMode == '4')
+        {
+            currMode = mode::writingForm;
 
-        titleForm.writeTitleForm();
-    }
+            TitleForm memoTemp;
 
-    else if (choosenMode == "4")
-    {
-        currMode = mode::writingForm;
+            memoTemp.writingMemo();
+        }
 
-        TitleForm memoTemp;
+        else if (choosenMode == '5')
+        {
+            currMode = mode::writingIntoDict;
 
-        memoTemp.writingMemo();
-    }
+            FileWritingReading fileWritingReading;
 
-    else if (choosenMode == "5")
-    {
-        currMode = mode::writingIntoDict;
+            fileWritingReading.dictionaryWriting();
+        }
 
-        FileWritingReading fileWritingReading;
+        currMode = mode::idle;
 
-        fileWritingReading.dictionaryWriting();
-    }
-
-    currMode = mode::idle;
-
-    //Statements for breaking the execution
-        cout << endl << "Press the Enter key to exit or any key to choose another mode\n";
+        //Statements for breaking the execution
+        cout << endl << "Press the Enter key to exit or any key to choose another mode" << endl;
+        //This statement is used to put the char symbol '\n' from an input
         cin.get(ch);
-        //cin.ignore(INT_MAX, '\n');
+        //Because the cin stores all inputted char symbols, this statement is preffered to cut others symbols
+        cin.ignore(INT_MAX, '\n');
     } while (ch != '\n');
 
     return 0;
