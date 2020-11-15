75d610002cc9710cdad966ea0a3a0cbcc6c36bd3
Dmitry <guise322@ya.ru>
2020-11-15 18:13:51 +0300

    Upgrade to the .net5 framework and implement support of https redirection

--- ProjectP/Server/Startup.cs
+++ ProjectP/Server/Startup.cs
@@ -50,10 +50,10 @@ namespace ProjectP.Server
                 app.UseExceptionHandler("/Error");
                 app.UseForwardedHeaders();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
